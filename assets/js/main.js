715d3f5557a21d67d7bc0935e0ec7751e8d5830f
BuguserOne <jinlingone@gmail.com>
2023-01-04 14:56:30 +0100

    Lev1_5_js-einführung_variables

---
+++ assets/js/main.js
@@ -0,0 +1,23 @@
+console.log("Starting")
+
+let carName = "BMW"
+console.log(carName)
+
+let x = 150
+console.log(x)
+
+let y = 50
+console.log(y)
+
+let z = x + y
+console.log(z)
+
+
+let firstName = "John"
+
+let lastName = "Doe"
+
+let age = 35
+
+document.write("Ich bin " + firstName + " " + lastName + " und bin " + age + "Jahre alt.")
+console.log("Ich bin " + firstName + " " + lastName + " und bin " + age + " Jahre alt.")
